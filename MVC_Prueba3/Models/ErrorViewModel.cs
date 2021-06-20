bac9a8115f989d51dd09a197ea701a1e93941b09
Diego Nicolás Pavez Acuña <diego.pavez@outlook.com>
2021-06-20 17:21:05 -0400

    Agregar archivos de proyecto.

---
+++ MVC_Prueba3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC_Prueba3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
