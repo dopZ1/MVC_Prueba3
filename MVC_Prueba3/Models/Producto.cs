da8088d1bddcc83577bcfb9126ca6f97d81716bf
Diego Nicolás Pavez Acuña <diego.pavez@outlook.com>
2021-06-20 20:50:56 -0400

    Producto.cs Agregado

---
+++ MVC_Prueba3/Models/Producto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Prueba3.Models
+{
+    public class Producto
+    {
+        public int ProductoId { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public double Precio { get; set; }
+        public int Stock { get; set; }
+    }
+}
